1b7f2a987de418d1dc253245792624a20b013e2f
Abdulaziz <abdusattorov2004@mail.ru>
2023-05-31 09:13:33 +0500

    starting

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php 
 
-bot('6077714195:AAG6CEXyLZjuBU08G2H5-GNr_-AE7KjVhDM');
+include 'Telegram.php';
 
-echo ' dsa';
+$telegram = new Telegram('6077714195:AAG6CEXyLZjuBU08G2H5-GNr_-AE7KjVhDM');
+
+$chat_id = $telegram->ChatID();
+$content = array('chat_id' => $chat_id, 'text' => 'Test');
+$telegram->sendMessage($content);

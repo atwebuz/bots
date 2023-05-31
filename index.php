74133ab4d9d37be88bd25ffd0cd576fe0062b1a7
Abdulaziz <abdusattorov2004@mail.ru>
2023-05-31 09:19:41 +0500

    /start -> someText

--- index.php
+++ index.php
@@ -6,5 +6,9 @@ $telegram = new Telegram('6077714195:AAG6CEXyLZjuBU08G2H5-GNr_-AE7KjVhDM');
 
 $chat_id = $telegram->ChatID();
 $text = $telegram->Text();
-$content = array('chat_id' => $chat_id, 'text' =>  $text);
-$telegram->sendMessage($content);
+
+
+if ($text == '/start'){
+    $content = array('chat_id' => $chat_id, 'text' =>  'Salom botmzga xush kelbsz!');
+    $telegram->sendMessage($content);
+}

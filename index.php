d9d8607290ba5fb3c7f873a30dac17349ffa4cd7
Abdulaziz <abdusattorov2004@mail.ru>
2023-05-31 09:16:12 +0500

    auto-text

--- index.php
+++ index.php
@@ -5,5 +5,6 @@ include 'Telegram.php';
 $telegram = new Telegram('6077714195:AAG6CEXyLZjuBU08G2H5-GNr_-AE7KjVhDM');
 
 $chat_id = $telegram->ChatID();
-$content = array('chat_id' => $chat_id, 'text' => 'Test');
+$text = $telegram->Text();
+$content = array('chat_id' => $chat_id, 'text' =>  $text);
 $telegram->sendMessage($content);

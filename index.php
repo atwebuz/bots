ccdedc58186eddf5ca7a92930838b69dd88052f7
Abdulaziz <abdusattorov2004@mail.ru>
2023-05-31 09:32:46 +0500

    keyboard_buttons

--- index.php
+++ index.php
@@ -9,9 +9,19 @@ $text = $telegram->Text();
 
 
 if ($text == '/start'){
-    $content = array('chat_id' => $chat_id, 'text' =>  'Assalomu aleykum auto shop botiga xush kelibsiz !!!');
+
+    $option = array(
+        //First row
+        array($telegram->buildKeyboardButton("Button 1"), $telegram->buildKeyboardButton("Button 2")), 
+        //Second row 
+        array($telegram->buildKeyboardButton("Button 3"), $telegram->buildKeyboardButton("Button 4"), $telegram->buildKeyboardButton("Button 5"))
+    );
+    $keyb = $telegram->buildKeyBoard($option, $onetime=false);
+   
+
+    $content = array('chat_id' => $chat_id,  'text' =>  'Assalomu aleykum auto shop botiga xush kelibsiz !!!');
     $telegram->sendMessage($content);
 
-    $content = array('chat_id' => $chat_id, 'text' =>  'Biz bilan boling va yetuk marralarga erishing.');
+    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' =>  'Biz bilan boling va yetuk marralarga erishing.');
     $telegram->sendMessage($content);
 }

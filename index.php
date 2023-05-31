70102301d9ab53881159a2276d123155d655c85c
Abdulaziz <abdusattorov2004@mail.ru>
2023-05-31 13:04:04 +0500

    go back btn

--- index.php
+++ index.php
@@ -3,15 +3,12 @@
 include 'Telegram.php';
 
 $telegram = new Telegram('6077714195:AAG6CEXyLZjuBU08G2H5-GNr_-AE7KjVhDM');
-// $fileFath = 'users/stap.txt';
-
-
 $data = $telegram->getData();
 $message = $data['message'];
-$telegram->sendMessage([
-    'chat_id' => $telegram->ChatID(),
-    'text' => json_encode($data, JSON_PRETTY_PRINT)
-]);
+// $telegram->sendMessage([
+//     'chat_id' => $telegram->ChatID(),
+//     'text' => json_encode($data, JSON_PRETTY_PRINT)
+// ]);
 
 $text = $data['message']['text'];
 $chat_id = $message['chat']['id'];
@@ -37,16 +34,6 @@ switch ($text){
     case "👨‍💻 Buyurtma berish":
         showOrder();
         break;
-    case "Orqaga":
-        switch(file_get_contents('users/stap.txt')){
-            case 'start':
-             break;
-            case 'order':
-                showStart();
-            break;
-            default;
-        }
-        break;    
     default:
         if (in_array($text, $orderTypes)){
             file_put_contents('users/mass.txt', $text);
@@ -72,7 +59,6 @@ switch ($text){
 function showStart(){
     global $telegram, $chat_id;
 
-    file_put_contents('users/stap.txt', 'start');
     $option = array(
         //First row
         array($telegram->buildKeyboardButton("🗣 Batafsil malumot")), 
@@ -99,7 +85,6 @@ function showAbout(){
 function showOrder(){
     global $telegram, $chat_id;
 
-    file_put_contents('users/phone.txt', 'order');
     $option = array(
         //First row
         array($telegram->buildKeyboardButton("Laccetti 15000$")),
@@ -108,9 +93,7 @@ function showOrder(){
         //Third row
         array($telegram->buildKeyboardButton("Spark 7300$")),
         //Fourth row 
-        array($telegram->buildKeyboardButton("Cobalt 8300$")).
-        // back
-        array($telegram->buildKeyboardButton("Orqaga"))
+        array($telegram->buildKeyboardButton("Cobalt 8300$"))
     );
     $keyb = $telegram->buildKeyBoard($option, $onetime=true,$resize=true);
    
